b591104776ea76e3c827f0cde6cecc7abe88af9b
cabaalexander <cabaalexander321@gmail.com>
2024-03-27 01:24:58 -0400

    execute pi5 remote

---
+++ README.md
@@ -0,0 +1,8 @@
+# pi-bootstrap.sh
+
+
+## pi5 init
+
+```
+curl -fsSL https://raw.githubusercontent.com/cabaalexander/pi-bootstrap.sh/main/pi5.sh | bash
+```
--- pi5.sh
+++ pi5.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-./init.sh
+_run(){
+  local REPO_URL file_path
+  REPO_URL="https://raw.githubusercontent.com/cabaalexander/pi-bootstrap.sh/main"
+  file_path=$(basename ${1:-})
+  curl -fsSL ${REPO_URL}/${file_path} | bash 
+}
 
-./coolify.sh
+_run ./init.sh
+
+_run ./coolify.sh
  
