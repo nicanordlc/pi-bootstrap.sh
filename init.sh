e895e9fe2d5815ead6d4b9ad7395a7a2503041a7
cabaalexander <cabaalexander321@gmail.com>
2024-07-29 12:12:44 -0400

    separate pi5 configuration from general pi settings

--- init.sh
+++ init.sh
@@ -9,13 +9,6 @@ _run(){
   curl -fsSL ${REPO_URL}/${file_path} | bash 
 }
 
-_copy(){
-  local file_path dst_path
-  file_path=$(basename ${1:-})
-  dst_path=${2:-}
-  curl -fsSL ${REPO_URL}/${file_path} > $dst_path
-}
-
 #apps
 sudo apt install -y \
   libimage-exiftool-perl \
@@ -25,15 +18,7 @@ sudo apt install -y \
   git \
   tmux
 
-# set a fixed DNS for the server
-sudo nmcli connection modify preconfigured ipv4.ignore-auto-dns yes
-sudo nmcli connection modify preconfigured ipv4.dns "1.1.1.1"
-sudo systemctl restart NetworkManager
-# check DNS: nmcli dev show | grep DNS
+#start
 
 _run ./docker.sh
-
-_copy \
-  ./config/etc-network-interfaces-d-main.conf \
-  /etc/network/interfaces.d/main.conf
 
--- pi5.sh
+++ pi5.sh
@@ -9,7 +9,26 @@ _run(){
   curl -fsSL ${REPO_URL}/${file_path} | bash 
 }
 
+_copy(){
+  local file_path dst_path
+  file_path=$(basename ${1:-})
+  dst_path=${2:-}
+  curl -fsSL ${REPO_URL}/${file_path} > $dst_path
+}
+
+#start
+
 _run ./init.sh
+
+# set a fixed DNS for the server
+sudo nmcli connection modify preconfigured ipv4.ignore-auto-dns yes
+sudo nmcli connection modify preconfigured ipv4.dns "1.1.1.1"
+sudo systemctl restart NetworkManager
+# check DNS: nmcli dev show | grep DNS
+
+_copy \
+  ./config/etc-network-interfaces-d-main.conf \
+  /etc/network/interfaces.d/main.conf
 
 _run ./coolify.sh
  
