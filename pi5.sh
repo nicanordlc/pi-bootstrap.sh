21538a7086be47f2b8a63591d86d41a3352f695b
cabaalexander <cabaalexander321@gmail.com>
2024-07-29 21:36:58 -0400

    add sudo to copy /etc directory

--- pi5.sh
+++ pi5.sh
@@ -9,26 +9,27 @@ _run(){
   curl -fsSL ${REPO_URL}/${file_path} | bash 
 }
 
-_copy(){
+_copy_sudo(){
   local file_path dst_path
-  file_path=$(basename ${1:-})
+  file_path=${1:-}
   dst_path=${2:-}
-  curl -fsSL ${REPO_URL}/${file_path} > $dst_path
+  curl -fsSL ${REPO_URL}/${file_path:1} | sudo tee $dst_path > /dev/null
 }
 
 #start
 
 _run ./init.sh
 
+_copy_sudo \
+  ./config/etc-network-interfaces-d-main.conf \
+  /etc/network/interfaces.d/main.conf
+
+_run ./coolify.sh
+ 
+# ### This needs to be run last since it will restart network manager
 # set a fixed DNS for the server
 sudo nmcli connection modify preconfigured ipv4.ignore-auto-dns yes
 sudo nmcli connection modify preconfigured ipv4.dns "1.1.1.1"
 sudo systemctl restart NetworkManager
 # check DNS: nmcli dev show | grep DNS
 
-_copy \
-  ./config/etc-network-interfaces-d-main.conf \
-  /etc/network/interfaces.d/main.conf
-
-_run ./coolify.sh
- 
