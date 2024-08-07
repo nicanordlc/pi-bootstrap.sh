9611e016af31b2daa471c65dbe06207f229f1bdb
cabaalexander <cabaalexander321@gmail.com>
2024-08-07 10:34:13 -0400

    add back `code-server` template + add gh-cli install script

---
+++ gh-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
